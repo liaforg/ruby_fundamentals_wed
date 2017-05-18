ef41348682d08b88a869b99036ba5930281990b6
Lia Forgione <liaforg@gmail.com>
2017-05-18 14:50:33 -0400

    up to exercise 10

--- exercise5.rb
+++ exercise5.rb
@@ -36,13 +36,64 @@ puts "---"
 
 # EXERCISE 8
 
-def expenses(my_expenses)
-  my_expenses = [250, 7.95, 30.95, 16.50]
+def my_expenses(expenses)
+  sum = expenses.reduce(0) do |expense, total|
+    total = total + expense
+  end
+  sum
 end
+
+puts add_expenses([250, 7.95, 30.95, 16.50])
 
 puts "---"
 
 # EXERCISE 9
 
+def groceries(list)
+  grocery_list = ["carrots", "peppers", "apples", "lemons"]
+  list.each do |item|
+    puts "* #{item}"
+  end
+end
 
-grocery_list = ["carrots", "peppers", "apples", "lemons"]
+
+grocery_list << "rice"
+grocery_list.count
+grocery_list.sort
+grocery_list.delete("lemons")
+
+
+# EXERCISE 10
+
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22,
+}
+
+def cohorts(students)
+  students.each do |name. numer|
+    puts "#{name} #{number} students"
+end
+
+students{:cohort4}=55
+
+puts students.keys
+
+students.delete(:cohort1)
+
+puts students
+
+sum = 0
+students.each do |name, number|
+  sum += number
+  puts sum
+
+staff {
+  :staff1 => 20
+  :staff2 => 30
+  :staff3 => 40
+  :staff4 => 50
+}
+
+puts staff
